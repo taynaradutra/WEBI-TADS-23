d022359f5029157eb7bdd4e8ff5b495335e0e0d8
taynaradutra <tay.dutra@hotmail.com>
2023-11-28 19:58:26 -0300

    projeto adoção - js formulários

---
+++ 12 - Eventos de Formulário/js/script.js
@@ -0,0 +1,37 @@
+let listaAnimais = [
+  {
+    idAnimal: 1,
+    nome: "Barão",
+    descricao:
+      "  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin diam     nisl, dapibus quis dictum quis, facilisis non risus. Aliquam tempor nisi id lacinia lobortis. Donec in justo mauris. Mauris luctustristique est, a faucibus elit. Vivamus et dapibus mi, id facilisisnunc. Etiam feugiat, sapien sed pharetra sagittis, risus sapien semper",
+    img: "./img/img1.webp",
+  },
+  {
+    idAnimal: 2,
+    nome: "Barão",
+    descricao:
+      "  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin diam     nisl, dapibus quis dictum quis, facilisis non risus. Aliquam tempor nisi id lacinia lobortis. Donec in justo mauris. Mauris luctustristique est, a faucibus elit. Vivamus et dapibus mi, id facilisisnunc. Etiam feugiat, sapien sed pharetra sagittis, risus sapien semper",
+    img: "./img/img2.jpg",
+  },
+  {
+    idAnimal: 3,
+    nome: "Barão",
+    descricao:
+      "  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin diam     nisl, dapibus quis dictum quis, facilisis non risus. Aliquam tempor nisi id lacinia lobortis. Donec in justo mauris. Mauris luctustristique est, a faucibus elit. Vivamus et dapibus mi, id facilisisnunc. Etiam feugiat, sapien sed pharetra sagittis, risus sapien semper",
+    img: "./img/img3.webp",
+  },
+  {
+    idAnimal: 4,
+    nome: "Barão",
+    descricao:
+      "  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin diam     nisl, dapibus quis dictum quis, facilisis non risus. Aliquam tempor nisi id lacinia lobortis. Donec in justo mauris. Mauris luctustristique est, a faucibus elit. Vivamus et dapibus mi, id facilisisnunc. Etiam feugiat, sapien sed pharetra sagittis, risus sapien semper",
+    img: "./img/img4.jpg",
+  },
+  {
+    idAnimal: 5,
+    nome: "Barão",
+    descricao:
+      "  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin diam     nisl, dapibus quis dictum quis, facilisis non risus. Aliquam tempor nisi id lacinia lobortis. Donec in justo mauris. Mauris luctustristique est, a faucibus elit. Vivamus et dapibus mi, id facilisisnunc. Etiam feugiat, sapien sed pharetra sagittis, risus sapien semper",
+    img: "./img/img5.webp",
+  },
+];
